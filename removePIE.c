72214a6118c184b2bce2350eb5b0f620003eaf0e
juliangrtz <julian.goeritz@student.uni-luebeck.de>
2024-01-24 03:24:36 +0100

    Remove unnecessary header file inclusions

--- removePIE.c
+++ removePIE.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include "xnu-definitions.h"
 
 void hexlify(unsigned char *data, uint32_t size) {
