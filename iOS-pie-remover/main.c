75b351eff0262e990d7a28a553a603f524b37d2a
juliangrtz <julian.goeritz@protonmail.com>
2025-10-08 23:22:55 +0200

    Improve 32/64-bit handling

--- iOS-pie-remover/main.c
+++ iOS-pie-remover/main.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mach-o/loader.h>
 
 void hexlify(unsigned char *data, uint32_t size) {
@@ -17,70 +18,106 @@ void hexlify(unsigned char *data, uint32_t size) {
 
 int main(int argc, char *argv[]) {
     FILE *editedFilePtr;
-    struct mach_header currentHeader;
-    
-    // error cases
-    if (argc < 1) {
-        puts("please enter the filename binary in the format ./removePIE filename");
+    uint32_t magic = 0;
+    int is64bit = 0;
+
+    if (argc < 2) {
+        puts("Usage: ./removePIE <mach-o_binary>");
         return EXIT_FAILURE;
     }
 
-    if ((editedFilePtr = fopen(argv[1], "rb+")) == NULL) {
-        printf("error, unable to open file %s\n", argv[1]);
+    editedFilePtr = fopen(argv[1], "rb+");
+    if (!editedFilePtr) {
+        printf("Error: Unable to open file %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-
-
-    if ((fread(&currentHeader.magic, sizeof(int32_t), 1, editedFilePtr)) == 0) {
-        puts("error reading magic constant in file");
-        return EXIT_FAILURE;
-    }
-
-    // check magic
-    if (currentHeader.magic == MH_MAGIC || currentHeader.magic == MH_MAGIC_64) {
-        puts("loading header...");
-        fseek(editedFilePtr, 0, SEEK_SET);
-        if ((fread(&currentHeader, sizeof(currentHeader), 1, editedFilePtr)) == 0) {
-            printf("error reading mach-o header");
-            return EXIT_FAILURE;
-        }
-        puts("looks ok");
-        printf("mach-o header: ");
-        hexlify((unsigned char *) &currentHeader, sizeof(currentHeader));
-
-        // everything ok. create a backup
-        puts("\n\nbacking up application binary...");
-        char cpCommand[1000];
-        sprintf(cpCommand, "cp %s %s.bak", argv[1], argv[1]);
-        system(cpCommand);
-        puts("done");
-
-        // modify mach-o header
-        printf("\noriginal flags:\t");
-        hexlify((unsigned char *) &currentHeader.flags, sizeof(currentHeader.flags));
-        printf("\ndisabling ASLR...\n");
-        currentHeader.flags &= ~MH_PIE;
-        printf("new flags:\t");
-        hexlify((unsigned char *) &currentHeader.flags, sizeof(currentHeader.flags));
-
-        fseek(editedFilePtr, 0, SEEK_SET);
-        if ((fwrite(&currentHeader, sizeof(char), 28, editedFilePtr)) == 0) {
-            printf("error writing to application file");
-        }
-
-        printf("\n\nASLR has been disabled for %s!\n", argv[1]);
-
-        // exit and close memory
+    if (fread(&magic, sizeof(uint32_t), 1, editedFilePtr) != 1) {
+        puts("Error: Unable to read magic number");
         fclose(editedFilePtr);
-        return EXIT_SUCCESS;
-    } else if (currentHeader.magic == MH_CIGAM || currentHeader.magic == MH_CIGAM_64) // big endian
-    {
-        puts("file is big endian, not an iOS binary!");
+        return EXIT_FAILURE;
+    }
+
+    if (magic == MH_MAGIC) {
+        puts("Detected: 32-bit Mach-O");
+        is64bit = 0;
+    } else if (magic == MH_MAGIC_64) {
+        puts("Detected: 64-bit Mach-O");
+        is64bit = 1;
+    } else if (magic == MH_CIGAM || magic == MH_CIGAM_64) {
+        puts("Error: Big-endian Mach-O (not supported)");
+        fclose(editedFilePtr);
         return EXIT_FAILURE;
     } else {
-        puts("file is not a Mach-O binary!");
+        puts("Error: Not a valid Mach-O file. What are you even doing?");
+        fclose(editedFilePtr);
         return EXIT_FAILURE;
     }
 
+    fseek(editedFilePtr, 0, SEEK_SET);
+
+    if (is64bit) {
+        struct mach_header_64 header;
+        if (fread(&header, sizeof(header), 1, editedFilePtr) != 1) {
+            puts("Error: Failed to read 64-bit header");
+            fclose(editedFilePtr);
+            return EXIT_FAILURE;
+        }
+
+        printf("Original flags: ");
+        hexlify((unsigned char *)&header.flags, sizeof(header.flags));
+        puts("");
+
+        char cpCommand[1024];
+        snprintf(cpCommand, sizeof(cpCommand), "cp \"%s\" \"%s.bak\"", argv[1], argv[1]);
+        system(cpCommand);
+        puts("Backup created.");
+
+        puts("Disabling PIE...");
+        header.flags &= ~MH_PIE;
+
+        printf("New flags: ");
+        hexlify((unsigned char *)&header.flags, sizeof(header.flags));
+        puts("");
+
+        fseek(editedFilePtr, 0, SEEK_SET);
+        if (fwrite(&header, sizeof(header), 1, editedFilePtr) != 1) {
+            puts("Error: Failed to write updated header");
+        } else {
+            printf("PIE has been disabled for %s (64-bit)\n", argv[1]);
+        }
+    } else {
+        struct mach_header header;
+        if (fread(&header, sizeof(header), 1, editedFilePtr) != 1) {
+            puts("Error: Failed to read 32-bit header");
+            fclose(editedFilePtr);
+            return EXIT_FAILURE;
+        }
+
+        printf("Original flags: ");
+        hexlify((unsigned char *)&header.flags, sizeof(header.flags));
+        puts("");
+
+        char cpCommand[1024];
+        snprintf(cpCommand, sizeof(cpCommand), "cp \"%s\" \"%s.bak\"", argv[1], argv[1]);
+        system(cpCommand);
+        puts("Backup created.");
+
+        puts("Disabling PIE...");
+        header.flags &= ~MH_PIE;
+
+        printf("New flags: ");
+        hexlify((unsigned char *)&header.flags, sizeof(header.flags));
+        puts("");
+
+        fseek(editedFilePtr, 0, SEEK_SET);
+        if (fwrite(&header, sizeof(header), 1, editedFilePtr) != 1) {
+            puts("Error: Failed to write updated header");
+        } else {
+            printf("PIE has been disabled for %s (32-bit)\n", argv[1]);
+        }
+    }
+
+    fclose(editedFilePtr);
+    return EXIT_SUCCESS;
 }
